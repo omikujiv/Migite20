d5a3d7ed614d05b483357dca113651e37de0a367
omikujiv <0394vtut@gmail.com>
2023-05-24 22:46:07 +0900

    Update_file(AutoComment)

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ DIY Keyboard that has 21 key and 3 encoder, for Right hand
 主に、お絵かきを想定
 
 驚異のロータリーエンコーダーを3積することで、様々な直観的な操作に対応。
+
+![IMGP3940](https://github.com/omikujiv/Migite20/assets/128278435/95e25fc7-f766-49bf-a8fe-ef074c9fde21)
+
--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -1,25 +1,53 @@
-// Copyright %(YEAR)s %(YOUR_NAME)s (@%(USER_NAME)s)
+// Copyright 2023 QMK
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include QMK_KEYBOARD_H
 
-// Defines names for use in layer keycodes and the keymap
-enum layer_names {
-    _BASE,
-    _FN
-};
+// ENCODER_MAP_ENABLE = yes
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /* Base */
-    [_BASE] = LAYOUT(
-        KC_1,    KC_2,    KC_3,    KC_4,
-        KC_5,    KC_6,    KC_7,    KC_8,
-        KC_9,    KC_0,    KC_A,    KC_B,
+    /*
+     * ┌───┬───┬───┬───┐
+     * │ 1 │ 2 │ 3 │ 4 │
+     * ├───┼───┼───┼───┤
+     * │ 5 │ 6 │ 7 │ 8 │
+     * ├───┼───┼───┤───│
+     * │ 9 │ 0 │ A │ B │
+     * ├───┼───┼───┼───┤
+     * │ C │ D │ E │ F │
+     * ├───┼───┼───┤───│
+     * │ G | H │ I │ J │ Z
+     * └───┴───┴───┴───┘
+     */
+    [0] = LAYOUT_numpad_5x4(
+        KC_P1,   KC_P2,   KC_P3,   KC_P4,
+        KC_P5,   KC_P6,   KC_P7,   KC_P8,
+        KC_P9,   KC_P0,   KC_A,    KC_B,
         KC_C,    KC_D,    KC_E,    KC_F,
-        KC_G,    KC_H,    KC_I,    KC_J
-    ),
-    [_FN] = LAYOUT(
-        _______, _______,  _______,
-            RESET,    XXXXXXX
+        KC_G,    KC_H,    KC_I,    KC_J, KC_Z
     )
 };
+
+
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == 0) { /* First encoder */
+        if (clockwise) {
+            tap_code(KC_K);
+        } else {
+            tap_code(KC_L);
+        }
+    } else if (index == 1) { /* Second encoder */
+        if (clockwise) {
+            tap_code(KC_N);
+        } else {
+            tap_code(KC_M);
+        }
+    } else if (index == 2) { /* Third encoder */
+        if (clockwise) {
+            tap_code(KC_O);
+        } else {
+            tap_code(KC_P);
+        }
+    }
+    return false;
+}
