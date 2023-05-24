14364706ff959d8dfe79fd33dcdc025766d198d9
omikujiv <0394vtut@gmail.com>
2023-05-24 22:31:46 +0900

    add_file

---
+++ Migite20.h
@@ -0,0 +1,29 @@
+// Copyright %(YEAR)s %(YOUR_NAME)s (@%(USER_NAME)s)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#include "quantum.h"
+
+/* This is a shortcut to help you visually see your layout.
+ *
+ * The first section contains all of the arguments representing the physical
+ * layout of the board and position of the keys.
+ *
+ * The second converts the arguments into a two-dimensional array which
+ * represents the switch matrix.
+ */
+#define LAYOUT( \
+    k00, k01, k02, k03,\
+    k10, k11, k12, k13,\
+    k20, k21, k22, k23,\
+    k30, k31, k32, k33,\
+    k40, k41, k42, k44,\
+    k51, k52, k53, k54\
+) { \
+    { k00, k01, k02, k03 }, \
+    { k10, k11, k12, k13 }, \
+    { k20, k21, k22, k23 }, \
+    { k30, k31, k32, k33 }, \
+    { k40, k41, k42, k43 } \
+}
